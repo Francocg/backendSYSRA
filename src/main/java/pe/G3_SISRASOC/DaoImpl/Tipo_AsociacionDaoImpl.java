9e7a26c8625ceed73b2e76721c621d41c2650f63
francocg <francocabanillas@upeu.edu.pe>
2020-12-10 21:43:17 -0500

    v1.1

--- src/main/java/pe/G3_SISRASOC/DaoImpl/Tipo_AsociacionDaoImpl.java
+++ src/main/java/pe/G3_SISRASOC/DaoImpl/Tipo_AsociacionDaoImpl.java
@@ -25,20 +25,35 @@ public class Tipo_AsociacionDaoImpl implements Tipo_AsociacionDao {
 		// TODO Auto-generated method stub
 		return jdbcTemplate.update("call pk_tipo_as.sp_create_tipo_as(?)", t.getNom_tipo());
 	}
-
 	@Override
 	public List<Map<String, Object>> readAll() {
 		// TODO Auto-generated method stub
-		/*List<Map<String,Object>> lista= new ArrayList<>();
+		List<Map<String,Object>> tipo = new ArrayList<>();
+		simpleJdbcCall = new SimpleJdbcCall(jdbcTemplate)
+				.withCatalogName("PKG_T_ASOCIACION") //nombre del paquete
+				.withProcedureName("SP_LIS_T_ASOCIACION") //nombre del procedimiento
+				.declareParameters(new SqlOutParameter("CUR_T_ASOCIACION", OracleTypes.REF_CURSOR, new ColumnMapRowMapper()));	
+				Map<String, Object> map = simpleJdbcCall.execute();
+				tipo.add(map);
+		return tipo;
+	}
+	
+	
+	
+	
+	/*@Override
+	public List<Map<String, Object>> readAll() {
+		// TODO Auto-generated method stub
+		List<Map<String,Object>> lista= new ArrayList<>();
 		simpleJdbcCall =new SimpleJdbcCall(jdbcTemplate)
 				.withCatalogName("pk_tipo_as")
 				.withProcedureName("sp_listar_tipo_as")
 				.declareParameters(new SqlOutParameter("cursor_tipo_as", OracleTypes.REF_CURSOR, new ColumnMapRowMapper()));
 		        Map<String, Object> map = simpleJdbcCall.execute();
 		        lista.add(map);
-		return lista;*/
+		return lista;
 		String sql ="select * from tipo_asociacion";
 		return jdbcTemplate.queryForList(sql);
-	}
+	}*/
 
 }
